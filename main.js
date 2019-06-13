2e19b5dd74baa15be703c4e24bc93a1ef04e2e36
HyoungHoKim <dkdemfp2@naver.com>
2019-06-13 18:07:39 +0900

    cloud change and json

--- main.js
+++ main.js
@@ -17,83 +17,87 @@ let tempp;
 let cur_city;
 
 function setup() {
-    createCanvas(960,520);
+  createCanvas(960, 520);
 
-    cur_city = CONSTANT.ARRAY.city[0];
+  cur_city = CONSTANT.ARRAY.city[0];
 
-    timebackground = new timeBackground(0.6);
-    timebackground.init();
+  timebackground = new timeBackground(0.6);
+  timebackground.init();
+  timebackground.calculateByTimeToSky(CONSTANT.VALUE.city_offset[cur_city]);
+  setInterval(function() {
     timebackground.calculateByTimeToSky(CONSTANT.VALUE.city_offset[cur_city]);
-    setInterval(function(){
-      timebackground.calculateByTimeToSky(CONSTANT.VALUE.city_offset[cur_city]);
-    }, 60000);
+  }, 60000);
 
-    tempp = new Temp();
+  tempp = new Temp();
 
-    hill = new Hill(0, 0);
+  hill = new Hill(0, 0);
 
-    totoro = new Totoro(CONSTANT.DIMEN.totoro_x, CONSTANT.DIMEN.totoro_y, CONSTANT.DIMEN.totoro_scale);
+  totoro = new Totoro(CONSTANT.DIMEN.totoro_x, CONSTANT.DIMEN.totoro_y, CONSTANT.DIMEN.totoro_scale);
 
-    dandalion = new Dandalion(CONSTANT.DIMEN.width, CONSTANT.DIMEN.height);
-    dandalion.init();
+  dandalion = new Dandalion(CONSTANT.DIMEN.width, CONSTANT.DIMEN.height);
+  dandalion.init();
+  dandalion.getBranchColor(CONSTANT.VALUE.city_offset[cur_city]);
+  setInterval(function() {
     dandalion.getBranchColor(CONSTANT.VALUE.city_offset[cur_city]);
-    setInterval(function(){
-      dandalion.getBranchColor(CONSTANT.VALUE.city_offset[cur_city]);
-    }, 60000);
+  }, 60000);
 
-    wheather = new Weather();
-    wheather.init();
+  wheather = new Weather();
+  wheather.init();
 
-    rain = new Rain();
-    rain.init();
+  rain = new Rain();
+  rain.init();
 
-    snow = new Snow();
+  snow = new Snow();
 
-    cloud = new Cloud();
+  cloud = new Cloud();
+  cloud.init();
 
-    // serial = new Serial();
-    // serial.init();
-    // serial.setMainSerialEventCallback(serialDataCallback);
+  // serial = new Serial();
+  // serial.init();
+  // serial.setMainSerialEventCallback(serialDataCallback);
 
-    wheather.loadWeatherData('Seoul', 0, setWheaterData);
+  wheather.loadWeatherData('Seoul', 0, setWheaterData);
 
-    info = new Info();
-    info.init(
-        {'x': 20, 'y': CONSTANT.DIMEN.height - 60},
-        {'x': CONSTANT.DIMEN.width - 150, 'y': -10}
-    );
+  info = new Info();
+  info.init({
+    'x': 20,
+    'y': CONSTANT.DIMEN.height - 60
+  }, {
+    'x': CONSTANT.DIMEN.width - 150,
+    'y': -10
+  });
 
-    // TODO: set city, time
-    info.setCityText(cur_city);
-    info.setDateText('JUN May 30 23:49');
+  // TODO: set city, time
+  info.setCityText(cur_city);
+  info.setDateText('JUN May 30 23:49');
 
-    setInterval(timebackground.calculateByTimeToSky(CONSTANT.VALUE.city_offset[cur_city]), 10000);
-    setInterval(dandalion.getBranchColor(CONSTANT.VALUE.city_offset[cur_city]), 10000);
+  setInterval(timebackground.calculateByTimeToSky(CONSTANT.VALUE.city_offset[cur_city]), 10000);
+  setInterval(dandalion.getBranchColor(CONSTANT.VALUE.city_offset[cur_city]), 10000);
 }
 
 function setWheaterData(data) {
-    print(data);
+  print(data);
 
-    cloud.init(data[0].clouds.all, data[0].wind.speed);
+  cloud.setCloudData(data[0].clouds.all, data[0].wind.speed);
 
-    tempp.init(data[0].main.temp);
+  tempp.init(data[0].main.temp);
 }
 
 function serialDataCallback(data) {
-    print('main serial data callback receive : ', data);
-    // wind, country
-    // changeCountry(city)
-    // blowDandalion(wind)
+  print('main serial data callback receive : ', data);
+  // wind, country
+  // changeCountry(city)
+  // blowDandalion(wind)
 }
 
 function changeCountry(city) {
-    wheather.loadWeatherData(city, 0, setWheaterData);
-    cur_city = CONSTANT.ARRAY.city[city];
-    info.setCityText(cur_city);
+  wheather.loadWeatherData(city, 0, setWheaterData);
+  cur_city = CONSTANT.ARRAY.city[city];
+  info.setCityText(cur_city);
 }
 
 function blowDandalion(wind) {
-    // dandalion.blow(wind);
+  // dandalion.blow(wind);
 }
 
 var IsSnow = false;
@@ -115,15 +119,15 @@ function draw() {
   totoro.drawTotoro();
   // blueTotoro.position(520, 120);
 
-  if( dandalion.getRainMode() ) {
+  if (dandalion.getRainMode()) {
     rain.draw();
-    blueTotoro.attribute('src', 'assets/blueTotoro_rain.gif');
-    blueTotoro = createImg('assets/blueTotoro_rain.gif');
+    //blueTotoro.attribute('src', 'assets/blueTotoro_rain.gif');
+    //blueTotoro = createImg('assets/blueTotoro_rain.gif');
   }
 
-  if( dandalion.getSnowMode() ) {
+  if (dandalion.getSnowMode()) {
     snow.draw();
-    blueTotoro.attribute('src', 'assets/blueTotoro_snow.gif');
+    //blueTotoro.attribute('src', 'assets/blueTotoro_snow.gif');
   }
 
 
